9c700330567eece95ee7a4a178f64912c1364654
Nicholas Mayne <nmayne@gmail.com>
2016-07-11 21:53:46 +0200

    finalize day 9 initial check in day 10

---
+++ Day10 - Elves Look, Elves Say/main10.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    print "just ran main"
+    final_val = create_permutation([0, 1, 2, 3, 4, 5])
+    print(str(final_val))
+    print("amount of lists = {0}".format(len(final_val)))
+
+
+if __name__ == "__main__":
+    main()
--- Day9 - All in a Single Night/main9.py
+++ Day9 - All in a Single Night/main9.py
@@ -1,5 +1,4 @@
 def create_permutation(number_list):
-    print(str(number_list))
     list_to_return = []
     if len(number_list) == 2:
         list_to_return = [list([number_list[0], number_list[1]]), list([number_list[1], number_list[0]])]
@@ -18,10 +17,48 @@ def create_permutation(number_list):
 
 
 def main():
-    print "just ran main"
-    final_val = create_permutation([0, 1, 2, 3, 4, 5])
-    print(str(final_val))
-    print("amount of lists = {0}".format(len(final_val)))
+    f = open("realInput.txt")
+    distanceData = {}
+    unique_list = []
+    for line in f:
+        split_line = line.split()
+        from_city = split_line[0]
+        to_city = split_line[2]
+        if from_city not in unique_list:
+            unique_list.append(from_city)
+
+        if to_city not in unique_list:
+            unique_list.append(to_city)
+
+        distance = int(split_line[4])
+        distanceData[(from_city, to_city)] = distance
+
+    final_val = create_permutation(unique_list)
+    shortestTrip = []
+    shortestDistance = 0
+
+    for trip in final_val:
+        tripLength = 0
+        from_city = ''
+        to_city = ''
+        for leg in trip:
+            if from_city == '' and to_city == '':
+                to_city = leg
+                continue
+            else:
+                from_city = to_city
+                to_city = leg
+
+            if (from_city, to_city) in distanceData:
+                tripLength += distanceData[(from_city, to_city)]
+            else:
+                tripLength += distanceData[(to_city, from_city)]
+
+        if tripLength > shortestDistance:
+            shortestTrip = trip
+            shortestDistance = tripLength
+
+    print("shortest trip is {0} miles, {1}".format(shortestDistance, str(shortestTrip)))
 
 
 if __name__ == "__main__":
