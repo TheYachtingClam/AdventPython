4ffd905c0fb5b1145a12adf45d228fe572d39813
Nicholas Mayne <nmayne@gmail.com>
2016-07-12 01:23:35 +0200

    created rules to determine password validity

--- Day11 - Corporate Policy/main11.py
+++ Day11 - Corporate Policy/main11.py
@@ -3,20 +3,67 @@ def next_password(last_password):
 
 
 def straight_rule(password):
-    pass
+    last_char = ' '
+    series_length = 0
+    for ch in password:
+        if series_length is 3:
+            break
+
+        if ord(last_char) + 1 is ord(ch):
+            last_char = ch
+            series_length += 1
+        else:
+            last_char = ch
+            series_length = 1
+
+    if series_length is 3:
+        return True
+
+    return False
 
 
 def mistaken_rule(password):
-    pass
+    if 'i' in password:
+        return False
+    if 'o' in password:
+        return False
+    if 'l' in password:
+        return False
+    return True
 
 
 def non_overlapping_rule(password):
-    pass
+    last_char = ' '
+    number_of_pairs = 0
+    for ch in password:
+        if number_of_pairs >= 2:
+            break
+        if last_char is ch:
+            number_of_pairs += 1
+            last_char = ' '
+        else:
+            last_char = ch
+
+    if number_of_pairs >= 2:
+        return True
+
+    return False
+
+
+def check_rules(password):
+    if not straight_rule(password):
+        return False
+    if not mistaken_rule(password):
+        return False
+    if not non_overlapping_rule(password):
+        return False
+    return True
 
 
 def main():
     f = open("testInput.txt")
 
+    print(str(check_rules("ghjaabcc")))
     for line in f:
         print(next_password(line))
 
