b405d1f6680e0848a2a2aecb3db0500172272a79
Nicholas Mayne <nmayne@gmail.com>
2016-07-13 01:32:19 +0200

    finish first half of day 12

--- Day12 - JSAbacusFramework.io/main12.py
+++ Day12 - JSAbacusFramework.io/main12.py
@@ -1,7 +1,29 @@
+class MyObj(object):
+    data = {}
 
 
 def main():
-    f = open("testInput.txt")
+    f = open("realInput.txt")
+    for line in f:
+        count = 0
+        currentNumber = ""
+        isNegative = False
+        for ch in line:
+            if ch is '-':
+                isNegative = True
+            elif ch.isdigit():
+                currentNumber += ch
+            elif currentNumber.isdigit():
+                if isNegative:
+                    count -= int(currentNumber)
+                else:
+                    count += int(currentNumber)
+                currentNumber = ""
+                isNegative = False
+            else:
+                isNegative = False
+
+        print("{0}, {1}".format(count, line.strip()))
 
 if __name__ == "__main__":
     main()
