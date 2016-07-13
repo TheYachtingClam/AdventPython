a12292180a8c3f1f8ca1f6774d84b605f491b078
Nicholas Mayne <nmayne@gmail.com>
2016-07-13 23:32:46 +0200

    Half way through the second half of 12, JSON encodiing almost done.  (works with bugs)

--- Day12 - JSAbacusFramework.io/main12.py
+++ Day12 - JSAbacusFramework.io/main12.py
@@ -1,29 +1,67 @@
-class MyObj(object):
+class ListNode(object):
+    parent = None
+    myList = []
+
+
+class ObjectNode(object):
+    parent = None
     data = {}
 
 
-def main():
-    f = open("realInput.txt")
-    for line in f:
-        count = 0
-        currentNumber = ""
-        isNegative = False
-        for ch in line:
-            if ch is '-':
-                isNegative = True
-            elif ch.isdigit():
-                currentNumber += ch
-            elif currentNumber.isdigit():
-                if isNegative:
-                    count -= int(currentNumber)
-                else:
-                    count += int(currentNumber)
-                currentNumber = ""
-                isNegative = False
-            else:
-                isNegative = False
+def AddComplexObject(node, lastNode, objName=""):
+    node.parent = lastNode
+    if isinstance(lastNode, ListNode):
+        lastNode.myList.append(node)
+    elif isinstance(lastNode, ObjectNode):
+        lastNode.data[objName] = node
 
-        print("{0}, {1}".format(count, line.strip()))
+def strOrNumber(strValue):
+    if strValue.isdigit():
+        return int(strValue)
+    else:
+        return strValue
+
+
+def main():
+    f = open("testInput.txt")
+    latestNode = ListNode()
+    latestItem = ""
+    itemName = ""
+    for bob in f:
+        for ch in bob:
+            if ch is "[":
+                tempNode = ListNode()
+                AddComplexObject(tempNode, latestNode)
+                latestNode = tempNode
+            elif ch is "{":
+                tempNode = ObjectNode()
+                AddComplexObject(tempNode, latestNode, itemName)
+                latestNode = tempNode
+            elif ch is "]":
+                latestNode.myList.append(latestItem)
+                latestItem = ""
+                latestNode = latestNode.parent
+            elif ch is "}":
+                latestNode.data[itemName] = latestItem
+                latestItem = ""
+                latestNode = latestNode.parent
+            elif ch is ":":
+                itemName = latestItem
+                latestItem = ""
+            elif ch is ",":
+                if isinstance(latestNode, ListNode) and latestItem is not "":
+                    latestNode.myList.append(strOrNumber(latestItem))
+                    latestItem = ""
+                elif isinstance(latestNode, ObjectNode) and latestItem is not "":
+                    latestNode.data[itemName] = strOrNumber(latestItem)
+                    latestItem = ""
+                    itemName = ""
+            else:
+                if ch is not "\"" and ch is not "\n":
+                    latestItem += ch
+
+        print("{0}, {1}".format(bob, str(latestNode)))
+
 
 if __name__ == "__main__":
     main()
